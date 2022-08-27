c285dc48c355d1239c93a18ac5ca846f8b017251
Kapsonlu Bebe <39134642+kapsonlubebe@users.noreply.github.com>
2022-08-27 16:21:58 -0400

    Update paswdgeneratorv1tr.py

--- paswdgeneratorv1tr.py
+++ paswdgeneratorv1tr.py
@@ -1,9 +1,12 @@
 import random
 import string
+mport colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+print(Fore.CYAN + Style.BRIGHT + "**************************")
+print(Fore.RED+ Style.BRIGHT +"Şifre oluşturucusuna hoşgeldiniz...")
 
-print("Şifre oluşturucusuna hoşgeldiniz...")
-
-length = int(input('\nŞifre uzunluğunu belirleyiniz :'))
+length = int(input(Fore.YELLOW + Style.BRIGHT + '\nŞifre uzunluğunu belirleyiniz :'))
 
 lower = string.ascii_lowercase
 upper = string.ascii_uppercase
@@ -16,22 +19,21 @@ temp = random.sample(all,length)
 
 password = "".join(temp)
 
-print("Yeni şifren : " , password)
-print("\n**************************")
-answer = input("-Yeni şifreni beğendin mi? 'E/h' :")
+print(Fore.GREEN + Style.BRIGHT +"Yeni şifren : " , password)
+print(Fore.CYAN + Style.BRIGHT +"\n**************************")
+answer = input(Fore.MAGENTA + Style.BRIGHT +"-Yeni şifreni beğendin mi? 'E/h' :")
 
 while answer == 'h':
-  
+
   temp = random.sample(all,length)
   password = "".join(temp)
-  print("\n**************************")
-  print("Yeni şifren : " , password)
-  answer = input("-Yeni şifreni beğendin mi? 'E/h' :")
-  
+  print(Fore.CYAN + Style.BRIGHT +"\n**************************")
+  print(Fore.GREEN + Style.BRIGHT +"Yeni şifren : " , password)
+  answer = input(Fore.GREEN + Style.BRIGHT +"-Yeni şifreni beğendin mi? 'E/h' :")
+
   if answer == 'E' and answer == 'e':
     break
-  
-  elif answer != 'E' and answer != 'e' and answer != 'h':
-    print("\n**************************")
-    answer = input("Geçerli bir cevap giriniz : 'E/h'")
 
+  elif answer != 'E' and answer != 'e' and answer != 'h':
+    print(Fore.RED+ Style.BRIGHT +"\n**************************")
+    answer = input(Fore.RED+ Style.BRIGHT +"Geçerli bir cevap giriniz : 'E/h'")
